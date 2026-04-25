df85e81aa9fc9b2528b53fd228ee05784e1e42c4
Victor Oyewole <theniceguytech@gmail.com>
2026-04-25 18:19:09 +0100

    update

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ KEYWORDS = ["Python Developer"]
 LINKEDIN_LOCATION = "Remote"
 ADZUNA_LOCATION   = ""        # empty = search all US
 
-MAX_PAGES   = 2
+MAX_PAGES   = 1
 DB_NAME     = "jobs.db"
-DATE_FILTER = "24h" # options: "24h", "3d", "7d", "any"
+DATE_FILTER = "1h" # options: "1h", "24h", "3d", "7d", "any"
 
 ADZUNA_APP_ID  = os.getenv("ADZUNA_APP_ID")
 ADZUNA_APP_KEY = os.getenv("ADZUNA_APP_KEY")
